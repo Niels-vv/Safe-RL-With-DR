2d681d1a8223f0ab54f55f6838a0a89cf0b2b6e0
Niels-vv <nielsvv@msn.com>
2021-11-10 13:03:35 +0100

    Setup vae testing for mnist

--- vae/VAE.py
+++ vae/VAE.py
@@ -4,6 +4,10 @@ from torch.nn import functional as F
 import torch.optim as optim
 import numpy as np
 
+import matplotlib.pyplot as plt
+import torchvision
+import torchvision.datasets as datasets
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Based on https://github.com/AntixK/PyTorch-VAE/blob/master/models/vanilla_vae.py
@@ -27,13 +31,13 @@ class VAE(nn.Module):
             hidden_dims = [32, 64, 128, 256, 512]
 
         # Build Encoder
-        for h_dim in hidden_dims:
-            modules.append(
-                nn.Sequential(
-                    nn.Linear(in_channels, h_dim),
-                    nn.LeakyReLU())
-            )
-            in_channels = h_dim
+        # for h_dim in hidden_dims:
+        #     modules.append(
+        #         nn.Sequential(
+        #             nn.Linear(in_channels, h_dim),
+        #             nn.LeakyReLU())
+        #     )
+        #     in_channels = h_dim
 
         #self.encoder = nn.Sequential(*modules)
         c_hid = 32
@@ -41,61 +45,44 @@ class VAE(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(1, c_hid, kernel_size=3, padding=1, stride=2), # 32x32 => 16x16
             act_fn(),
-            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
-            act_fn(),
-            nn.Conv2d(c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 16x16 => 8x8
-            act_fn(),
-            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
-            act_fn(),
-            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1, stride=2), # 8x8 => 4x4
-            act_fn(),
             nn.Flatten()
         )
 
         #self.fc_mu = nn.Linear(hidden_dims[-1], latent_dim)
         #self.fc_var = nn.Linear(hidden_dims[-1], latent_dim)
-        self.fc_mu = nn.Linear(2*16*c_hid, latent_dim)
-        self.fc_var = nn.Linear(2*16*c_hid, latent_dim)
+        self.fc_mu = nn.Linear(256*c_hid, latent_dim)
+        self.fc_var = nn.Linear(256*c_hid, latent_dim)
 
 
         # Build Decoder
         modules = []
 
-        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1])
+        # self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1])
 
-        hidden_dims.reverse()
+        # hidden_dims.reverse()
 
-        for i in range(len(hidden_dims) - 1):
-            modules.append(
-                nn.Sequential(
-                    nn.Linear(hidden_dims[i], hidden_dims[i+1]),
-                    nn.LeakyReLU())
-            )
+        # for i in range(len(hidden_dims) - 1):
+        #     modules.append(
+        #         nn.Sequential(
+        #             nn.Linear(hidden_dims[i], hidden_dims[i+1]),
+        #             nn.LeakyReLU())
+        #     )
 
 
 
         #self.decoder = nn.Sequential(*modules)
         self.linear = nn.Sequential(
-            nn.Linear(latent_dim, 2*16*c_hid),
+            nn.Linear(latent_dim, 256*c_hid),
             act_fn()
         )
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(2*c_hid, 2*c_hid, kernel_size=3, output_padding=1, padding=1, stride=2), # 4x4 => 8x8
-            act_fn(),
-            nn.Conv2d(2*c_hid, 2*c_hid, kernel_size=3, padding=1),
-            act_fn(),
-            nn.ConvTranspose2d(2*c_hid, c_hid, kernel_size=3, output_padding=1, padding=1, stride=2), # 8x8 => 16x16
-            act_fn(),
-            nn.Conv2d(c_hid, c_hid, kernel_size=3, padding=1),
-            act_fn(),
             nn.ConvTranspose2d(c_hid, 1, kernel_size=3, output_padding=1, padding=1, stride=2), # 16x16 => 32x32
-            nn.Tanh() # The input images is scaled between -1 and 1, hence the output has to be bounded as well
         )
-        self.final_layer = nn.Sequential(
-                            nn.Linear(hidden_dims[-1],hidden_dims[-1]),
-                            nn.LeakyReLU(),
-                            nn.Linear(hidden_dims[-1],out_channels),
-                            nn.Tanh())
+        # self.final_layer = nn.Sequential(
+        #                     nn.Linear(hidden_dims[-1],hidden_dims[-1]),
+        #                     nn.LeakyReLU(),
+        #                     nn.Linear(hidden_dims[-1],out_channels),
+        #                     nn.Tanh())
 
     def encode(self, input):
         """
@@ -104,11 +91,14 @@ class VAE(nn.Module):
         :param input: (Tensor) Input tensor to encoder [N x C x H x W]
         :return: (Tensor) List of latent codes
         """
+        print(f'Shape initial:{input.shape}')
         result = self.encoder(input)
+        print(f'Shape after conv encode flatten {result.shape}')
         #result = torch.flatten(result, start_dim=1)
         # Split the result into mu and var components
         # of the latent Gaussian distribution
         mu = self.fc_mu(result)
+        print(f'Shape after linear to latent {mu.shape}')
         log_var = self.fc_var(result)
         return [mu, log_var]
 
@@ -123,9 +113,14 @@ class VAE(nn.Module):
         ##result = result.view(-1, 512, 2, 2)
         #result = self.decoder(result)
         #result = self.final_layer(result)
+        print(f'Shape before decode {z.shape}')
         x = self.linear(z)
-        x = x.reshape(x.shape[0], -1, 4, 4)
+        print(f'Shape after linear {x.shape}')
+        x = x.reshape(x.shape[0], 1, 16, 16)
+        print(f'Shape after reshape {x.shape}')
         result = self.decoder(x)
+        print(f'Shape after decoding {result.shape}')
+        raise NotImplementedError
         return result
 
     def reparameterize(self, mu, logvar):
@@ -173,47 +168,78 @@ class VAE(nn.Module):
 
 
 class VaeManager():
-  def __init__(self, vae_model, optimizer, obs_file, batch_size, latent_space, vae_lr):
-    self.vae_model = vae_model
-    self.optimizer = optimizer
-    self.obs_file = obs_file
-    self.batch_size = batch_size
-    self.latent_space = latent_space
-    self.vae_lr = vae_lr
+    def __init__(self, vae_model, optimizer, obs_file, batch_size, latent_space, vae_lr):
+        self.vae_model = vae_model
+        self.optimizer = optimizer
+        self.obs_file = obs_file
+        self.batch_size = batch_size
+        self.latent_space = latent_space
+        self.vae_lr = vae_lr
 
-  def train_step(self, batch):
-    reconstruction, input, mu, log_var = self.vae_model(batch)
-    loss = self.vae_model.loss_function(reconstruction, input, mu, log_var)['loss']
-    self.optimizer.zero_grad()
-    loss.backward()
-    self.optimizer.step()
-    return loss
+    def train_step(self, batch):
+        reconstruction, input, mu, log_var = self.vae_model(batch)
+        loss = self.vae_model.loss_function(reconstruction, input, mu, log_var)['loss']
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        print(f'Batch done. Loss: {loss}')
+        return loss
 
-  def train_on_trace(self, obs):
-    batch = []
-    i = 0
-    for index, row in obs.iterrows():
-        i += 1
-        row = row.to_numpy()
-        if self.vae_model.conv:
-            row = row.reshape(32,32) # TODO this is pysc2 specific
-            row = np.expand_dims(row, 0)  
-        batch.append(row)
-        if len(batch) % self.batch_size == 0:
-            batch = np.array(batch)
-            #self.train_step(torch.tensor(batch, dtype=torch.float, device=device))
-            self.train_step(torch.from_numpy(batch).to(device).float())
-            batch = []
-    print("donezo")
+    def train_on_trace(self, obs):
+        batch = []
+        i = 0
+        for index, row in obs.iterrows():
+            i += 1
+            row = row.to_numpy()
+            if self.vae_model.conv:
+                row = row.reshape(32,32) # TODO this is pysc2 specific
+                row = np.expand_dims(row, 0)  
+            batch.append(row)
+            if len(batch) % self.batch_size == 0:
+                batch = np.array(batch)
+                #self.train_step(torch.tensor(batch, dtype=torch.float, device=device))
+                self.train_step(torch.from_numpy(batch).to(device).float())
+                batch = []
+        print("donezo")
 
-  def state_dim_reduction(self, state):
-    return self.vae_model.state_dim_reduction(state).squeeze()
+    def state_dim_reduction(self, state):
+        return self.vae_model.state_dim_reduction(state).squeeze()
 
-  def get_checkpoint(self):
-    return {'model_state_dict' : self.vae_model.state_dict(),
-            'optimizer_state_dict': self.optimizer.state_dict(),
-            'latent_space' : self.latent_space,
-            'vae_lr' : self.vae_lr,
-            'batch_size' : self.batch_size,
-            'obs_file' : self.obs_file
-    }
+    def get_checkpoint(self):
+        return {'model_state_dict' : self.vae_model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'latent_space' : self.latent_space,
+                'vae_lr' : self.vae_lr,
+                'batch_size' : self.batch_size,
+                'obs_file' : self.obs_file
+        }
+
+    def train_test_set(self,input):
+        i = 0
+        while i + self.batch_size < len(input):
+            print(f'Traning images {i} - {i+self.batch_size}.')
+            batch = input[i:i+self.batch_size]
+            self.train_step(batch)
+            i += self.batch_size
+
+    def recons_test(self, input):
+        for i in range(5):
+            image = input[i:i+1]
+            reconstruction, input, mu, log_var = self.vae_model(image)
+            loss = self.vae_model.loss_function(reconstruction, input, mu, log_var)['loss']
+            print(f'Loss: {loss}')
+            fig = plt.figure
+            plt.imshow(input[i], cmap='gray')
+            plt.show()
+            fig = plt.figure
+            plt.imshow(reconstruction, cmap='gray')
+            plt.show()
+
+if __name__ == "__main__":
+    mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+    mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+    vae_model = VAE(in_channels = 2, latent_dim = 16*16).to(device)
+    vae_optimizer = optim.Adam(params=vae_model.parameters(), lr=0.001)
+    vae_manager = VaeManager(vae_model, vae_optimizer, f'env_pysc2/results_vae/{map}', 100, 16*16, 0.001)
+    vae_manager.train_test_set(mnist_trainset)
+    vae_manager.recons_test(mnist_testset)
